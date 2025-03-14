db7c7db18fac43ec64238022d84080451496d979
satyabuddharajuvenkata <buddharaju.varma@icloud.com>
2025-03-13 23:46:50 -0500

    StringInterpolation

---
+++ jsStringInterpolation.js
@@ -0,0 +1,11 @@
+console.log("String Interpolation");
+
+let myString1 = "my name is";
+let myString2 = "Satya Buddharaju";
+let myString3 = "I Stay in";
+let myString4 = "Katy";
+let calculateAge = 2025 - 1980;
+let myString5 = "age is ";
+
+let myFinalString = `my Name is ${myString2}. I stay in ${myString4} and my age is ${calculateAge}.`;
+console.log(myFinalString); //my Name is Satya Buddharaju. I stay in Katy and my age is 45.
